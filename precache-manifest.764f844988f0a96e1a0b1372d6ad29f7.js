a1d07963c91c2c8d552be5c7658c28ceb1ec3bc8
Celeste <celeste.denaday@gmail.com>
2020-08-07 21:02:37 +0200

    second

---
+++ precache-manifest.764f844988f0a96e1a0b1372d6ad29f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4104628e22b3a501b11e989d2ef549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328c1139d4be37800122",
+    "url": "/static/css/main.8f962885.chunk.css"
+  },
+  {
+    "revision": "f80fe8467c6c6f7ef4bf",
+    "url": "/static/js/2.ba10d245.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ba10d245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328c1139d4be37800122",
+    "url": "/static/js/main.d8c1c885.chunk.js"
+  },
+  {
+    "revision": "fa15da177897ef9cef8c",
+    "url": "/static/js/runtime-main.958a13e0.js"
+  }
+]);
